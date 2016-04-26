6185ddfacfd7a5b9144841131da9ff3e8c45966f
finley hebert-perkins <fhebert-perkins16@bayschoolsf.org>
2016-04-26 12:56:25 -0700

    pictures

--- serverv1/server.py
+++ serverv1/server.py
@@ -4,17 +4,19 @@ import random
 import json
 
 COLORS = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 165, 0)",
-    "rgb(0, 0, 255)",
-    "rgb(0, 128, 0)",
-    "rgb(128, 0, 128)"
+    # "rgb(255, 0, 0)",
+    # "rgb(255, 165, 0)",
+    # "rgb(0, 0, 255)",
+    # "rgb(0, 128, 0)",
+    # "rgb(128, 0, 128)",
+    "url('archer.png')"
 ]
 
 USERS = []
 
 async def test(websocket, path):
     entry = {
+        "uid": "".join(random.choice("abcdefghijklmnopqrtstuvwxyz1234567890") for i in range(5)),
         "websocket":websocket,
         "color":random.choice(COLORS),
         "state":None
@@ -35,7 +37,7 @@ async def test(websocket, path):
                     await user["websocket"].send(json.dumps(response))
     except websockets.exceptions.ConnectionClosed:
         USERS.remove(entry)
-START_SERVER = websockets.serve(test, 'localhost', 8080)
+START_SERVER = websockets.serve(test, '0.0.0.0', 8080)
 
 asyncio.get_event_loop().run_until_complete(START_SERVER)
 asyncio.get_event_loop().run_forever()

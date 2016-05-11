df80206af6c2cfbe91f8400cb75e179d118b8f0f
finley hebert-perkins <fhebert-perkins16@bayschoolsf.org>
2016-05-10 20:45:13 -0700

    more changes

--- DigitalTabletop/webApp.py
+++ DigitalTabletop/webApp.py
@@ -4,45 +4,51 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug import secure_filename
 
 
-mapDict = {"size":[2, 2]}
-UPLOAD_FOLDER = 'static/'
-ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"])
+mapDict = {"size":[2, 2]} # Default map size is 2x2
+UPLOAD_FOLDER = 'static/' # this means that all files uploaded will be accessable at /static/<filename>
+ALLOWED_EXTENSIONS = set(["png", "jpg", "jpeg"]) # Allowed image extensions for stuff and things
 
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
+def allowed_file(filename): # return true if file is of allowed type
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def index():
-    return render_template("grid.html", MAP=mapDict)
+    return render_template("grid.html", MAP=mapDict) # Return the grid software thing
 
-@app.route("/dm", methods=["POST","GET"])
+@app.route("/dm", methods=["POST","GET"]) # map configuration
 def dm():
-    global mapDict
-    if request.method == "POST":
-        if request.form.get("background") != "":
+    global mapDict # allow local mapDict varialbe to influence global one.
+    if request.method == "POST": # if its a post request, process form
+        if request.form.get("background") != "": # incase background is not defined
             mapDict["background"] = request.form.get("background")
-        if request.form.get("size") != "":
+        if request.form.get("size") != "": # incase size is not defined
             mapDict["size"] = request.form.get("size").split(",")
         mapDict["props"] = {}
-        i = 1
-        for key in request.form.keys():
-            if "prop"+str(i) == key:
-                mapDict["props"][key] = {
-                    "image" : request.form.get(key+".image"),
-                    "sizeX" : request.form.get(key+".size").split(",")[0],
-                    "sizeY" : request.form.get(key+".size").split(",")[1],
-                    "x": request.form.get(key+".position").split(",")[0],
-                    "y": request.form.get(key+".position").split(",")[1]
-                }
-    files = os.listdir(app.config["UPLOAD_FOLDER"])
+
+        pn = 1 # this is the prop number
+        try: # incase props arent defined
+            for key in request.form.keys():
+                if "prop"+str(pn) == key:
+                    mapDict["props"][key] = {
+                        "image" : request.form.get(key+".image"),
+                        "sizeX" : request.form.get(key+".size").split(",")[0],
+                        "sizeY" : request.form.get(key+".size").split(",")[1],
+                        "x": request.form.get(key+".position").split(",")[0],
+                        "y": request.form.get(key+".position").split(",")[1]
+                    }
+                    pn+=1 # update the iterator i to make sure that the prop number is correct
+        except IndexError: # if there is not a correctly defined prop, ignore the error
+            pass
+
+    files = os.listdir(app.config["UPLOAD_FOLDER"]) # Show all images...
     return render_template("dm.html", files=files)
 
-@app.route("/assets", methods=["POST", "GET"])
+@app.route("/assets", methods=["POST", "GET"]) # Upload new files
 def assets():
     if request.method == 'POST':
         file = request.files['file']
